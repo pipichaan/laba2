f362ad57ac1f495c873beeb548a8876f0537e936
pipichaan <147584671+pipichaan@users.noreply.github.com>
2023-10-16 21:26:44 +0300

    Add files via upload
    Широкова Алина 23ит35

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите номер месяца");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x == 12 || x <= 2)
+{
+    Console.WriteLine("Зима");
+}
+else if (x >= 3 && x <= 5)
+{
+    Console.WriteLine("Весна");
+}
+else if (x >= 6 && x <= 8)
+{
+    Console.WriteLine("Лето");
+}
+else if (x >= 9 && x <= 11)
+{
+    Console.WriteLine("Осень");
+}
+
+
